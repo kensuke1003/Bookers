572d8f46a9edb3302b29d18ba487f0c737b6565e
kensuke1003 <kensukeseven1003@gmail.com>
2021-02-03 10:22:55 +0000

    commit4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  
   get 'top' => 'top#top'
   resources :books
-
-  
 end
